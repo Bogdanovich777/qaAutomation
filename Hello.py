5f96582e6230ffe77d8a171f399e810546960f3a
Bogdanovich777 <alligatoro1987@gmail.com>
2018-10-07 15:35:10 +0300

    bad

--- Hello.py
+++ Hello.py
@@ -1,2 +1,11 @@
-print("Hello World!")
+import subprocess
 
+s = 'adb devices'
+
+f = subprocess.Popen(s, shell=True)
+
+f.communicate(s)
+
+
+
+#-s shell input 505 505
---
+++ try_login_on_site/pogoda.py
@@ -0,0 +1,26 @@
+import requests
+
+url = 'https://e.mail.ru/login'
+s = requests.session()
+r = s.get(url)
+act_token = r.cookies['act_token']
+
+
+
+data = {
+    'Login' : 'alligatoro',
+    'Password' : '37951qwedAnn',
+    'saveauth' : '1',
+    'new_auth_form' : '1',
+    'FromAccount' : 'opener%3Dmail.login%26allow_external%3D1',
+    'page' : 'https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Fback%3D1%26back%3D1%26from%3Dmail.login',
+    'act_token' : act_token,
+    'back' : '1',
+    'Domain' : 'mail.ru'
+}
+
+auth = s.post(url, data=data)
+
+print(r.status_code)
+
+print(r.text)
