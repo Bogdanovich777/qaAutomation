89d8b1f51991840d0a4895e06223f1d783ffb9df
Bogdanovich777 <alligatoro1987@gmail.com>
2018-10-07 22:12:14 +0300

    bad

--- try_login_on_site/pogoda.py
+++ try_login_on_site/pogoda.py
@@ -1,22 +1,22 @@
 import requests
 
-url = 'https://e.mail.ru/login'
+url = 'https://mail.ru/login'
 s = requests.session()
-r = s.get(url)
-act_token = r.cookies['act_token']
+r = s.get(url, verify=False)
+act_token = r.cookies['act']
 
 
 
 data = {
     'Login' : 'alligatoro',
     'Password' : '37951qwedAnn',
-    'saveauth' : '1',
-    'new_auth_form' : '1',
-    'FromAccount' : 'opener%3Dmail.login%26allow_external%3D1',
-    'page' : 'https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Fback%3D1%26back%3D1%26from%3Dmail.login',
-    'act_token' : act_token,
-    'back' : '1',
-    'Domain' : 'mail.ru'
+#    'saveauth' : '1',
+#    'new_auth_form' : '1',
+#    'FromAccount' : 'opener%3Dmail.login%26allow_external%3D1',
+#    'page' : 'https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Fback%3D1%26back%3D1%26from%3Dmail.login',
+    'act' : act_token,
+#    'back' : '1',
+#    'Domain' : 'mail.ru'
 }
 
 auth = s.post(url, data=data)
