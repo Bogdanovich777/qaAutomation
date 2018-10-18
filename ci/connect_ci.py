4c5f6f0c99d6ac05201356209002db937bdcf31e
o.bogdanovich <obogdanovich@melsoft-games.com>
2018-10-18 17:25:17 +0300

    copy URL

--- ci/connect_ci.py
+++ ci/connect_ci.py
@@ -5,6 +5,15 @@ import requests
 
 
 
+url = 'https://artifactory.mgsn.it/webapp/#/builds/mycafe-client-android-git'
+
+path = input(' пиши тут: ')
+
+sss = requests.request("GET", 'https://artifactory.mgsn.it/webapp/#/builds/mycafe-client-android-git/{0}'.format(path))
+rer = sss.url
+print(rer)
+
+
 
 
 f = open('D:/Melesta/QA/123.apk', 'wb')
