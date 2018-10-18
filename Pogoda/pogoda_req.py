fde93f6be020ad2bbb10e564a70d09921744ffe0
o.bogdanovich <obogdanovich@melsoft-games.com>
2018-10-18 12:59:09 +0300

    Download build

--- Pogoda/pogoda_req.py
+++ Pogoda/pogoda_req.py
@@ -7,34 +7,40 @@ url = 'http://6.pogoda.by/' # url
 r = requests.get(url)
 
 
-with open ('pogoda.html', 'wb') as file:
+with open('pogoda.html', 'wb') as file:
     file.write(r.text.encode('cp1251'))     #Сохранили страничку в виде HTML
-    file.close()
+
 
 
 file = open('pogoda.html', 'rb')
 soup = BeautifulSoup(file, 'lxml')
 
 pogoda_list = soup.find("table", attrs={"id":"forecast"})
-pogoda2 = soup.find_all('tr')[9]
-
+#pogoda2 = soup.find_all('tr')[9]
+file.close()
 
 #print(pogoda_list)
-with open ('pogoda.html', 'wb') as file:
-    file.write(pogoda2.encode('cp1251'))
-    file.close()
+with open('pogoda.html', 'wb') as file:
+    file.write(pogoda_list.encode('cp1251'))
 
 file = open('pogoda.html', 'rb')
-pogoda3 = soup.find('tr')
+soup_result = BeautifulSoup(file, 'lxml')
+pogoda_s = soup_result.find('b')
+file.close()
 
-print(pogoda3)
+print(pogoda_s)
+
+with open('pogoda_result.html', 'wb') as file:
+    file.write(pogoda_s.encode('cp1251'))
+
+#file = open('pogoda.html', 'rb')
+#soup_result = BeautifulSoup(file, 'lxml')
+#pogoda_s = soup_result.find_all('b')
+#file.close()
+
+#print(pogoda_s)
 
 
-
-
-#with open ('pogoda.html', 'wb') as file:
-#    file.write(pogoda3.encode('cp1251'))
-#    file.close()
 #new_file = open('123.html', 'rb')
 #soup2 = BeautifulSoup(new_file, 'lxml')
 #pogoda2 = soup.find_all('tr')[6]
